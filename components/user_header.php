c1e80da8c0e1c7e52d3c5f03dfc317dabada2b65
Hebafaouri <126398763+Hebafaouri@users.noreply.github.com>
2023-05-18 22:56:46 +0300

    first commit

---
+++ components/user_header.php
@@ -0,0 +1,51 @@
+
+<header class="header">
+
+   <section class="flex">
+
+      <a href="home.php" class="logo">Shopie<span>.</span></a>
+
+      <nav class="navbar">
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="orders.php">orders</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </nav>
+
+      <div class="icons">
+        
+         <div id="menu-btn" class="fas fa-bars"></div>
+         <a href="search_page.php"><i class="fas fa-search"></i></a>
+         <a href="wishlist.php"><i class="fas fa-heart"></i><span>(<?= $total_wishlist_counts; ?>)</span></a>
+         <a href="cart.php"><i class="fas fa-shopping-cart"></i><span>(<?= $total_cart_counts; ?>)</span></a>
+         <div id="user-btn" class="fas fa-user"></div>
+      </div>
+
+      <div class="profile">
+        
+         <p><?= $fetch_profile["name"]; ?></p>
+         <a href="update_user.php" class="btn">update profile</a>
+         <div class="flex-btn">
+            <a href="user_register.php" class="option-btn">register</a>
+            <a href="user_login.php" class="option-btn">login</a>
+         </div>
+         <a href="components/user_logout.php" class="delete-btn" onclick="return confirm('logout from the website?');">logout</a> 
+         <?php
+            }else{
+         ?>
+         <p>please login or register first!</p>
+         <div class="flex-btn">
+            <a href="user_register.php" class="option-btn">register</a>
+            <a href="user_login.php" class="option-btn">login</a>
+         </div>
+         <?php
+            }
+         ?>      
+         
+         
+      </div>
+
+   </section>
+
+</header>
